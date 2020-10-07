cd079bcb08b981b5d88c295261693ce41be3f5f9
Forest Wu <forestwu@forests-mbp.myfiosgateway.com>
2020-10-07 19:46:05 -0400

    change camelcase to snake case, fix invert

--- findBestPathForGenshin.py
+++ findBestPathForGenshin.py
@@ -1,25 +1,24 @@
 import cv2
+import sys
 
-path = '/mnt/e/genshinGeoculusMap.jpg'
+path = sys.argv[1]
 
 #grayscale
 img = cv2.imread(path)
-grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 #convert non-blacks to be white
-(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 15, 255, cv2.THRESH_BINARY)
+(thresh, black_and_white_image) = cv2.threshold(gray_image, 15, 255, cv2.THRESH_BINARY)
 
-height, width, ___ = img.shape
+height, width = black_and_white_image.shape
 
 #invert it
-for row in range(0, height - 1):
-    for col in range(0, width -1 ):
-        pixel = img[row, col] 
-        for val in pixel:
-            val = 255 - val
-        
-        img[row, col] = pixel 
+inverted = ~black_and_white_image
 
-cv2.imshow('image',img)
+cv2.imshow('image',black_and_white_image)
+cv2.waitKey(0)
 
+#connected components
+#num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(grayImage, 8, cv2.CV_32S)
+#print(centroids)
 #tsp
