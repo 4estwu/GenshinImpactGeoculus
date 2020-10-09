96f6d84bc858aeaa67f6c008aefbb818af326b02
Forest Wu <forestliwu@gmail.com>
2020-10-08 23:54:51 -0400

    uploading image

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# GenshinImpactGeoculus
-shortest path to obtain all geoculus
+# Genshin Impact: Geoculus Pathing
+There are 131 Geoculus in Genshin Impact. Collecting all will unlock a hidden quest/dungeon.
+So I ran guided local search on this, and found a fast route to obtain all Geoculus. 
+Disclaimer: This route is not the fastest, as it does not take into account terrain/water. 
+
+Best of luck!
+Forest
--- geoculus_best_path.py
+++ geoculus_best_path.py
@@ -50,9 +50,10 @@ routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
 # use guided local search
 search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-search_parameters.first_solution_strategy = (
-    routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-
+search_parameters.local_search_metaheuristic = (
+    routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+search_parameters.time_limit.seconds = 30
+search_parameters.log_search = True
 # let google do all the hard work
 solution = routing.SolveWithParameters(search_parameters)
 
